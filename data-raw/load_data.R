747af6e0fa6d6fc8125c4007d849e729227bf4c7
MrKevinNa <drkevin2022@gmail.com>
2020-11-28 14:40:36 +0900

    edit load_data

--- data-raw/load_data.R
+++ data-raw/load_data.R
@@ -1,14 +1,18 @@
+## code to prepare `load_data` dataset goes here
 
 # 패키지 호출
 library(tidyverse)
+
+# 현재 작업경로 확인
+getwd()
 
 # 데이터 불러오기
 hitters <- readRDS(file = 'KBO_Hitters_2020.RDS')
 pitchers <- readRDS(file = 'KBO_Pitchers_2020.RDS')
 
 # tibble로 변환
-hitters <- as.tibble(x = hitters)
-pitchers <- as.tibble(x = pitchers)
+hitters <- as_tibble(x = hitters)
+pitchers <- as_tibble(x = pitchers)
 
 
 ## End of Document
